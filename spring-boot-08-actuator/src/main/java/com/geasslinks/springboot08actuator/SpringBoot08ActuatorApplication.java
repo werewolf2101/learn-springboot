976fc4a8f8a4fd18b280e43c7f9b3f1ba2acefcb
lei jin <werewolf2101@msn.com>
2018-11-05 15:23:28 +0800

    08

---
+++ spring-boot-08-actuator/src/main/java/com/geasslinks/springboot08actuator/SpringBoot08ActuatorApplication.java
@@ -0,0 +1,12 @@
+package com.geasslinks.springboot08actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot08ActuatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBoot08ActuatorApplication.class, args);
+    }
+}
